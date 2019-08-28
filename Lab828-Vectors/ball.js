9f795f86c71371e822ba898d1e190fca8b43c931
795308 <54591256+795308@users.noreply.github.com>
2019-08-28 08:43:56 -0700

    Firstvector

---
+++ Lab828-Vectors/ball.js
@@ -0,0 +1,36 @@
+class Ball{
+  constructor(x,y,dx,dy){
+    this.loc = createVector(x, y);
+    this.vel = createVector(dx, dy);
+    this.acc = createVector(0, 1);
+    this.clr = color(random(255),random(255),random(255));
+    this.w = random(10, 30);
+  }//constructor end
+  run(){
+    this.checkEdges();
+    this.update();
+    this.render();
+  }//run end
+  render(){
+    fill(this.clr);
+    ellipse(this.loc.x, this.loc.y, this.w, this.w)
+  }//render end
+  checkEdges(){
+    if(this.loc.x < 0){
+      this.vel.x = -this.vel.x
+    }
+    if(this.loc.x > width){
+      this.vel.x = -this.vel.x
+    }
+    if(this.loc.y < 0){
+      this.vel.y = -this.vel.y
+    }
+    if(this.loc.y > height){
+      this.vel.y = -this.vel.y
+    }
+  }//checkEdges end
+  update(){
+    this.vel.add(this.acc);
+    this.loc.add(this.vel);
+  }//update end
+}//Class end

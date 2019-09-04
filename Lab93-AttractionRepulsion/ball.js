6c3b12ac76b4cf250d9ff2ba65bad6698f4f4df9
795308 <54591256+795308@users.noreply.github.com>
2019-09-04 08:50:21 -0700

    9/4/19 Class

--- Lab93-AttractionRepulsion/ball.js
+++ Lab93-AttractionRepulsion/ball.js
@@ -9,7 +9,7 @@ class Ball{
     if(this.id < 0){this.w = 50;}
   }//constructor end
   run(){
-    if(this.id < 0){this.checkEdges();}
+    this.checkEdges();
     this.update();
     this.render();
   }//run end
@@ -18,24 +18,26 @@ class Ball{
     ellipse(this.loc.x, this.loc.y, this.w, this.w)
   }//render end
   checkEdges(){
-    if(this.loc.x < 0){
-      this.vel.x = -this.vel.x
-    }
-    if(this.loc.x > width){
-      this.vel.x = -this.vel.x
-    }
-    if(this.loc.y < 0){
-      this.vel.y = -this.vel.y
-    }
-    if(this.loc.y > height){
-      this.vel.y = -this.vel.y
+
+      if(this.loc.x < 0){
+        this.loc.x = width
+      }
+      if(this.loc.x > width){
+        this.loc.x = 0
+      }
+      if(this.loc.y < 0){
+        this.loc.y = height
+      }
+      if(this.loc.y > height){
+        this.loc.y = 0
+      
     }
   }//checkEdges end
   update(){
     var distToMainball;
     if(this.id >= 0){
       distToMainball = this.loc.dist(mainBall.loc);
-      if(distToMainball < 10000000){
+      if(distToMainball < 250){
         //add attraction
         this.acc = p5.Vector.sub(mainBall.loc, this.loc);
         this.acc.normalize();

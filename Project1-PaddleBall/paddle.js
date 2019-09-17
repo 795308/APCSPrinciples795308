3e46ae4dd2ce17471fc399f25925086ffb8c2512
795308 <54591256+795308@users.noreply.github.com>
2019-09-16 17:30:04 -0700

    Home 9/16/19

--- Project1-PaddleBall/paddle.js
+++ Project1-PaddleBall/paddle.js
@@ -4,6 +4,7 @@ class Paddle{
     this.w = 150
     this.h = 20
     this.clr = color(127);
+    this.score = 0
   }//constructor end
   run(){
     this.render();
@@ -15,7 +16,7 @@ class Paddle{
     rect(this.loc.x, this.loc.y, this.w, this.h);
   }//creates paddle object
   update(){
-    var mouseLoc = createVector(ball.loc.x - this.w/2, this.loc.y);
+    var mouseLoc = createVector(mouseX, this.loc.y);
     this.loc = p5.Vector.lerp(this.loc, mouseLoc, .09);
   }//moves paddle to mouseX
   checkEdges(){

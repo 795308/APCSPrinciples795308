5f85ad52f344f536285fbe49f6641398860a6bdd
795308 <54591256+795308@users.noreply.github.com>
2019-10-10 08:25:05 -0700

    Class 10/10/19

--- Lab1009-BubbleSort/sketch.js
+++ Lab1009-BubbleSort/sketch.js
@@ -2,12 +2,15 @@
 // 	10/9/19
 //  This is a comment
 //  The setup function function is called once when your program begins
-var list = [6,3,5,2,4,8,7,10,18,19,14,1]
+var list = [6,3,5,2,4,1]
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
+  for(var i = 0; i < list.length; i++){
+    console.log(list[i]);
+  }
   BubbleSort();
 
 }
@@ -16,8 +19,11 @@ function BubbleSort(){
   for(var j = 0; j < list.length-1; j++){
     for(var i = 0; i < list.length-1; i++){
       if(list[i] > list[i+1]){
-        swap(list, i, i+1)
+        swap(list, i, i+1);
       }
+    }
+    for(var i = 0; i < list.length; i++){
+      console.log(list[i]);
     }
   }
 }

c18f9860a6e79e05bbc338e3f18944a995cf2853
795308 <54591256+795308@users.noreply.github.com>
2019-10-17 08:50:45 -0700

    Class 10/17/19

---
+++ Lab1017-SelectionSort/sketch.js
@@ -0,0 +1,32 @@
+//  Nico McCarten
+// 	10/17/19
+//  This is a comment
+//  The setup function function is called once when your program begins
+var list = [];//Array containing the numbers
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(5, 5, 5);
+  fill(200, 30, 150);
+  for(var i = 0; i < 2500; i++){
+  list[i] = i;
+}
+shuffle(list, true);
+console.log(list);
+InsertionSort();
+
+}
+
+function swap(list, a, b){
+  var temp = list[a];
+  list[a] = list[b];
+  list[b] = temp;
+}
+
+function InsertionSort(){
+  for(var j = 0; j < list.length-1; j++){
+    for(var i = j; i > 0; i--){
+      if(list[i] )
+    }
+  }
+}
